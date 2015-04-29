51e2e235b2d17f2aa5c76b28f43cf6806f1dfdd0
Daniel Gempesaw <gempesaw@gmail.com>
2015-04-29 12:36:36 -0400

    Add documentation to the main module

--- README.md
+++ README.md
@@ -10,7 +10,48 @@ version 0.01
 
 # SYNOPSIS
 
+    use DDP;
+    my $hq = Honeydew::Queue->new;
+    p $hq->get_queues;
+
+    $hq->drop_nightly_queues;
+
 # DESCRIPTION
+
+This module is a thin wrapper around [Resque](https://metacpan.org/pod/Resque) that implements
+Honeydew-specific queue functionality that we'd like.
+
+# ATTRIBUTES
+
+## resque
+
+Optional: By default, we'll instantiate a Resque client for the one
+specified by [Honeydew::Config](https://metacpan.org/pod/Honeydew::Config); you can also pass in a [Resque](https://metacpan.org/pod/Resque)
+object of your choosing if you'd like to mock tests, or use your own
+Resque without specifying a config file. See the tests for how to do
+that - in particular, the `before each` section of
+`Honeydew-Queue.t`.
+
+## config
+
+We expects an instance of [Honeydew::Config](https://metacpan.org/pod/Honeydew::Config); you can inject your own
+with the proper settings, but you're probably better served just
+giving us a resque object above.
+
+# METHODS
+
+## get\_queues ()
+
+Accepts no args; returns a hashref. The keys are the names of the
+queues, and the values are the size of each queue.
+
+## drop\_nightly\_queues ()
+
+Accepts no args; returns this object for lack of anything better to
+do. There's no error handling in place. We'll look up the nightly
+queues from the `local` entry in our [Honeydew::Config](https://metacpan.org/pod/Honeydew::Config), and drop
+only the nightly ones. This is useful for resetting your queue to a
+clean state before starting a new batch of tests.
 
 # BUGS
 
--- lib/Honeydew/Queue.pm
+++ lib/Honeydew/Queue.pm
@@ -12,7 +12,27 @@ use Resque;
 
 =head1 SYNOPSIS
 
+    use DDP;
+    my $hq = Honeydew::Queue->new;
+    p $hq->get_queues;
+
+    $hq->drop_nightly_queues;
+
 =head1 DESCRIPTION
+
+This module is a thin wrapper around L<Resque> that implements
+Honeydew-specific queue functionality that we'd like.
+
+=cut
+
+=attr resque
+
+Optional: By default, we'll instantiate a Resque client for the one
+specified by L<Honeydew::Config>; you can also pass in a L<Resque>
+object of your choosing if you'd like to mock tests, or use your own
+Resque without specifying a config file. See the tests for how to do
+that - in particular, the C<before each> section of
+C<Honeydew-Queue.t>.
 
 =cut
 
@@ -31,10 +51,25 @@ has resque => (
     }
 );
 
+=attr config
+
+We expects an instance of L<Honeydew::Config>; you can inject your own
+with the proper settings, but you're probably better served just
+giving us a resque object above.
+
+=cut
+
 has config => (
     is => 'lazy',
     default => sub { return Honeydew::Config->instance; }
 );
+
+=method get_queues ()
+
+Accepts no args; returns a hashref. The keys are the names of the
+queues, and the values are the size of each queue.
+
+=cut
 
 sub get_queues {
     my ($self) = @_;
@@ -48,6 +83,16 @@ sub get_queues {
     return \%queue_jobs;
 }
 
+=method drop_nightly_queues ()
+
+Accepts no args; returns this object for lack of anything better to
+do. There's no error handling in place. We'll look up the nightly
+queues from the C<local> entry in our L<Honeydew::Config>, and drop
+only the nightly ones. This is useful for resetting your queue to a
+clean state before starting a new batch of tests.
+
+=cut
+
 sub drop_nightly_queues {
     my ($self) = @_;
 
@@ -56,6 +101,8 @@ sub drop_nightly_queues {
     foreach ($self->_nightly_queues) {
         $self->resque->remove_queue($_);
     }
+
+    return $self;
 }
 
 sub _nightly_queues {
