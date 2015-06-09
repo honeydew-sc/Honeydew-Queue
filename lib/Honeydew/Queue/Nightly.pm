26de9ccc17dbd17bf9fdae5682bab961e665ab71
Daniel Gempesaw <gempesaw@gmail.com>
2015-06-09 13:50:56 -0400

    Refactor expected sets as an attribute

--- lib/Honeydew/Queue/Nightly.pm
+++ lib/Honeydew/Queue/Nightly.pm
@@ -3,12 +3,14 @@ package Honeydew::Queue::Nightly;
 # ABSTRACT: Accumulate sets and features for nightly enqueueing
 use Moo;
 use feature qw/state/;
+use File::Spec;
 use Honeydew::Config;
 
 has dbh => (
     is => 'lazy',
     default => sub {
-        my $settings = Honeydew::Config->instance->{mysql};
+        my ($self) = @_;
+        my $settings = $self->config->{mysql};
 
         require DBI;
         # require DBD::mysql;
@@ -25,18 +27,29 @@ has dbh => (
     }
 );
 
-sub get_sets_to_be_queued {
-    my ($self) = @_;
-    my $dbh = $self->dbh;
+has config => (
+    is => 'lazy',
+    default => sub { return Honeydew::Config->instance }
+);
 
-    my $expected_sets = $self->expected_sets;
-    my $actual_sets = $self->actual_sets;
+has sets_to_run => (
+    is => 'lazy',
+    default => sub {
+        my ($self) = @_;
+        my $dbh = $self->dbh;
 
-    my $set_count = _set_count($expected_sets, [ values %{ $actual_sets } ] );
-    my $missing_sets = _get_missing($set_count);
+        my $expected_sets = $self->expected_sets;
+        my $actual_sets = $self->actual_sets;
 
-    return $missing_sets;
-}
+        my $set_count = _set_count($expected_sets, [ values %{ $actual_sets } ] );
+        my $missing_sets = _get_missing($set_count);
+
+        return $missing_sets;
+    }
+);
+
+
+
 
 sub expected_sets {
     my ($self) = @_;
